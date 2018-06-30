2f4ae5cdfbc3fe8a26061e4d49242c72b38a661e
Carlos López <yo@carlop.es>
2018-06-30 13:45:52 +0200

    Spring Data JPA query methods

--- src/main/java/es/carlop/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/es/carlop/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package es.carlop.spring5recipeapp.controllers;
 
+import es.carlop.spring5recipeapp.domain.Category;
+import es.carlop.spring5recipeapp.domain.UnitOfMeasure;
+import es.carlop.spring5recipeapp.repositories.CategoryRepository;
+import es.carlop.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("index");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional =unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println(("Cat Id is: " + categoryOptional.get().getId()));
+        System.out.println(("Uom Id is: " + unitOfMeasureOptional.get().getId()));
+
         return  "index";
     }
 }
--- src/main/java/es/carlop/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/es/carlop/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package es.carlop.spring5recipeapp.repositories;
 import es.carlop.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
