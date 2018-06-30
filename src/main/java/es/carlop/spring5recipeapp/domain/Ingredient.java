4c8da707e40ffd9750fae7f6456ee62ed185eb55
Carlos López <yo@carlop.es>
2018-06-30 11:14:43 +0200

    Assigment - Create One to One Relationship
    I have to create a One to One relationship between recipe and unit of measure

--- src/main/java/es/carlop/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/es/carlop/spring5recipeapp/domain/Ingredient.java
@@ -15,6 +15,9 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
     public Long getId() {
         return id;
     }
