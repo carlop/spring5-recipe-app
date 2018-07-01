b191dfe5778b594e37d832385d5956ec13eb85fb
Carlos López <yo@carlop.es>
2018-07-01 14:21:52 +0200

    Assignment: Display List of Recipes on Index Page

---
+++ src/main/java/es/carlop/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,9 @@
+package es.carlop.spring5recipeapp.services;
+
+import es.carlop.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
