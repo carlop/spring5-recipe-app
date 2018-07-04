2364e428263fde3ce1c7f2066d7bc992d3ef917d
Carlos López <yo@carlop.es>
2018-07-05 00:51:43 +0200

    Creating a JUnit Test

---
+++ src/test/java/es/carlop/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,35 @@
+package es.carlop.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp (){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4l;
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+        String descriptionValue = "Descripción de prueba";
+        category.setDescription(descriptionValue);
+        assertEquals(descriptionValue, category.getDescription());
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+}
