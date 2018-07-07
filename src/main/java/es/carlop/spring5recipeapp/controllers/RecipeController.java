6358e1b51de9c33185531a45640785c4df359e07
Carlos López <yo@carlop.es>
2018-07-07 21:21:29 +0200

    Create a Recipe

--- src/main/java/es/carlop/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/es/carlop/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package es.carlop.spring5recipeapp.controllers;
 
+import es.carlop.spring5recipeapp.commands.RecipeCommand;
 import es.carlop.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -25,5 +28,25 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return  "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+
+        log.debug("Creating a new recipe!");
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+
     }
 }
