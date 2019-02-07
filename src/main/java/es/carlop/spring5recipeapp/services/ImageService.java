cf936aad32852c53638f91217dbd3dc62ddc5da7
Carlos López <yo@carlop.es>
2019-02-07 12:13:17 +0100

    Uploading Images with Spring MVC

---
+++ src/main/java/es/carlop/spring5recipeapp/services/ImageService.java
@@ -0,0 +1,7 @@
+package es.carlop.spring5recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
