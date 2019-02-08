b65924592f8e63d044a56dc4335b0157849b5a0a
Carlos López <yo@carlop.es>
2019-02-08 13:02:16 +0100

    Spring MVC Exception Handler

--- src/main/java/es/carlop/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/es/carlop/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package es.carlop.spring5recipeapp.controllers;
 
 import es.carlop.spring5recipeapp.commands.RecipeCommand;
+import es.carlop.spring5recipeapp.exceptions.NotFoundException;
 import es.carlop.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -63,4 +66,16 @@ public class RecipeController {
 
         return "redirect:/";
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
+    }
+
 }
