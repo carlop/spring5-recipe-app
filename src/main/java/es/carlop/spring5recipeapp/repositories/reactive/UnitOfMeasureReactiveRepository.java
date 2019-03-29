a766e7e37145cfaed5bf4f29af689ffe90541f78
Carlos López <yo@carlop.es>
2019-03-29 11:56:13 +0100

    Reactive Repositories

---
+++ src/main/java/es/carlop/spring5recipeapp/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,7 @@
+package es.carlop.spring5recipeapp.repositories.reactive;
+
+import es.carlop.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+}
